610d4ab9fff7a852e81355e752a8b411aa81956b
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-04-26 22:46:50 +0900

    PDCAが回るようになった

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Pdca from "./components/Pdca.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Pdca />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Helvetica", "Arial", "Hiragino Kaku Gothic ProN",
+    "ヒラギノ角ゴ ProN W3", "メイリオ", Meiryo, "ＭＳ Ｐゴシック",
+    "MS PGothic" sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  line-height: 1px;
 }
 </style>
---
+++ src/components/Pdca.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="pdca">
+    <p>PD</p>
+    <p>CA</p>
+  </div>
+</template>
+<style>
+.pdca {
+  font-size: 200px;
+  animation: 0.01s linear 0s infinite revolution;
+}
+@-webkit-keyframes revolution {
+  from {
+    transform: rotate(0turn);
+  }
+  to {
+    transform: rotate(1turn);
+  }
+}
+</style>
